2c1fc084cf570d7c385c7c22c6a6e27835432e0a
Gabriel Ozaki <gfozaki+github@gmail.com>
2016-10-28 21:53:07 -0200

    O compilador ja comeca fazer a analise lexida da linguagem PS, lendo os codigos disponibilizados pelo professor

--- Compilador/src/compilador/AnalisadorLexico.java
+++ Compilador/src/compilador/AnalisadorLexico.java
@@ -13,26 +13,70 @@ import java.util.List;
  * @author gabrielozaki
  */
 public class AnalisadorLexico {
-    
-    public static void AnalisadorLexico(){
+
+    public static void AnalisadorLexico() {
         //So cria
     }
-   
-    public List<Token> analisa(String str){
+
+    public List<Token> analisa(String str) {
         //Cria uma lista de tokens, eles são inicializados com os lexiomas mais informações referentes(ver construtor)
         List<Token> tokens = new ArrayList<>();
-        
+
         //Aqui vamos dividir os elementos em tokens nao classificados
         //String temporaria para auxiliar no laço, ela vai basicamente servir para armazenar numeros, maiores informações no laço
-        String tmp="";
+        String tmp = "";
         //auxiliares para armazenar as linhas e colunas
-        int i=1;
-        int ini=1;
-        int linha=1;
-        
+        int i = 1;
+        int ini = 1;
+        int linha = 1;
+        //Boleano para verificar se o lexima contem apenas numeros
+        boolean numero;
+
         //Percorre toda string original caractere a caractere
-        for(char ch: str.toCharArray())
-        {
+        for (char ch : str.toCharArray()) {
+            //Espacos podem ser ignorados na analise lexica
+            if (ch != ' ' && ch != '\t') {
+                //Aqui salvamos sequenciamente todos os caracteres ate encontrar algum delimitador
+                //Nesse caso ele aceitaria um a.braco.fawofa.flka mas isso sera dado erro na classificacao lexica 
+                if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '.') {
+                    tmp += ch;
+                } else {
+                    //Verificamos se existe um conteudo em tmp, se existir adiciona aos tokens nao classificados
+                    //Sinal que a interaçao anterior pegou um numero
+                    if (tmp != "") {
+                        tokens.add(new Token(tmp, linha, ini, i - 1));
+                    }
+
+                    //Limpa o tmp
+                    tmp = "";
+                    ini = i;
+                    //ignoramos os espacos em branco
+                    if (ch != ' ') {
+                        //Pegamos a quebra de linha, sendo que esta define o final de uma equacao
+                        if (ch == '\n') {
+                            linha++;
+                            //Volta ini para 1 pois e uma nova linha
+                            ini = 1;
+                            //o incremento ocorre no final do laco, entao setamos para 0 para ele ser incrementado para 1
+                            i = 0;
+                        } else {
+                            //Adicionamos o simbolo, problema é que é necessário converter char para string porque o java nao faz automaticamente
+                            tokens.add(new Token(String.valueOf(ch), linha, ini, i));
+                            //Ini recebe i+1 pois estamos preparando o ini do proximo numero
+                            ini = i + 1;
+                        }
+                    }
+                }
+
+            } else if (tmp != "") {
+                //Consideramos o espaco como um delimitador entre tokens, porem ele e ignorado
+                //por isso aqui ocorre um caso especial, em que o delimitador nao e adicionado aos tokens
+                tokens.add(new Token(tmp, linha, ini, i - 1));
+                tmp = "";
+            }
+            //Prepara o i para proxima interacao
+            i++;
+            /* 
           //Comparaçao simples de um caractere nao justifica uso de regex
           if( (ch >= '0' && ch <= '9') || ch == '.' )
           {
@@ -71,8 +115,9 @@ public class AnalisadorLexico {
         //Repetimos esse trecho pois o ultimo valor pode ser um numero nao inserido na tabela
         if(tmp != "")
             tokens.add(new Token(tmp,linha,ini,i-1));
-     
+             */
+        }
         return tokens;
     }
-       
+
 }
--- Compilador/src/compilador/Token.java
+++ Compilador/src/compilador/Token.java
@@ -14,17 +14,74 @@ public class Token {
     //Os tipos sao enumeraveis, para facilitar sua utilizacao
     public static enum Tipo{
         //O tipo de token recebe como parametro o seu regex
+        
+        //BNF Utilizada:
+        //http://www2.fct.unesp.br/docentes/dmec/olivete/compiladores/arquivos/LALG.pdf
+        //Palavras reservadas(ex:comandos), como os testes ocorrem em ordem do tipo, entao inserimos as reservadas primeiro
+        //Declaracao de programa 1
+        Programa("program"),
+        //Declaracao de variavel 4
+        Variavel("var"),
+        //Declaracao de procedimento 8
+        Procedure("procedure"),
+        //Declaracao de tipos OBS da pag 1
+        //NOTA isso e diferente de Numero_Inteiro, um e a declaracao e o outro e o numero em si
+        //Nos codigos de exemplo o professor usou int ao inves de integer, logo aceitaremos ambos
+        Inteiro("integer|int"),
+        Real("real"),
+        Booleano("boolean"),
+        //Valores que um booleano pode assumir, uso isolado tambem e comum
+        Valor_Boleano("true|false"),
+        //Declaracao de comandos de leitura escrita OBS da pag 1
+        Leitura("read"),
+        Escrita("write"),
+        //Declaracao de comando composto 11
+        Composto_inicio("begin"),
+        Composto_fim("end"),
+        //Declaracao de condicao 15
+        Condicional("if"),
+        Condicionalt("then"),
+        Condicionale("else"),
+        //Declaracao do repeticao 16
+        Repeticao("while"),
+        Repeticaod("do"),
+        //Declaracao de relacoes 18
+        //NOTA nao inserimos <> <= e >= porque essa analise composta cabe ao analisador sintatico
+        //Ex: declarar que =< e errado
+        Igual("="),
+        Maior(">"),
+        Menor("<"),
+        
+        //Operadores logicos 20 
+        And("and"),
+        Or("or"),
+        Div("div"),
+        Not("not"),
+        
+        //identificando simbolos
+        Dois_Pontos(":"),
+        Ponto_Virgula(";"),
+        Virgula(","),
+        
+        //Comentarios
+        Chaves_Abre("\\{"),
+        Chaves_Fecha("\\}"),
         //Para Reais, esse regex exige que tenha pelo menos um numero antes do . e pelo menos um numero depois, logo .3 ou 2. nao sao validos
         Numero_Real("[0-9]+.[0-9]+"),
         //Para Inteiros, esse regex exige que tenha pelo menos um numero 
         Numero_Inteiro("[0-9]+"),
-        //Resto das operacoes apenas verifica o caractere
+        
+        //Operacoes
         Operador_Soma("[+]"),
         Operador_Subtracao("[-]"),
         Operador_Multiplicacao("[*]"),
         Operador_Divisao("[/]"),
         Parenteses_Abre("[(]"),
-        Parenteses_Fecha("[)]");
+        Parenteses_Fecha("[)]"),
+        
+        //Declaramos os identificadores
+        Identificador("(_|[a-z]|[A-Z])+(_|[a-z]|[A-Z]|[0-9])*");
+        
         
         private final String regex;
         
@@ -78,5 +135,12 @@ public class Token {
         if(erro)
             this.erro = Erro.Nao_Pertence_Alfabeto;
             
+    }
+    
+    public void listaTipo(){
+        for(Tipo t: Tipo.values())
+        {
+            System.out.println(t.name());
+        }
     }
 }
