52a2a467180a355a82c31b4ab1098796cb2a57f1
gabrielozaki <gfozaki+github@gmail.com>
2016-10-31 09:45:39 -0200

    Reorganizado o codigo da calculadora, tirando a logica de negocio da interface grafica

--- lexico/src/lexico/Editor.java
+++ lexico/src/lexico/Editor.java
@@ -156,58 +156,11 @@ public class Editor extends javax.swing.JFrame {
         //Trocar a textArea no futuro por um JPaneText?
         String str = Equacoes.getText();
 
-        //Cria uma lista de tokens, eles são inicializados com os lexiomas mais informações referentes(ver construtor)
-        List<Token> tokens = new ArrayList<>();
+        //Cria uma instancia do analisador lexico
+        AnalisadorLexico al = new AnalisadorLexico();
         
-        //Aqui vamos dividir os elementos em tokens nao classificados
-        //String temporaria para auxiliar no laço, ela vai basicamente servir para armazenar numeros, maiores informações no laço
-        String tmp="";
-        //auxiliares para armazenar as linhas e colunas
-        int i=1;
-        int ini=1;
-        int linha=1;
-        
-        //Percorre toda string original caractere a caractere
-        for(char ch: str.toCharArray())
-        {
-          //Comparaçao simples de um caractere nao justifica uso de regex
-          if( (ch >= '0' && ch <= '9') || ch == '.' )
-          {
-              //Aqui isolaremos os numeros, o ponto é adicionado por causa dos reais
-              tmp += ch;
-              
-          }else{
-              //Verificamos se existe um conteudo em tmp, se existir adiciona aos tokens nao classificados
-              //Sinal que a interaçao anterior pegou um numero
-              if(tmp != "")
-                  tokens.add(new Token(tmp,linha,ini,i-1));
-     
-              //Limpa o tmp
-              tmp = "";
-              ini = i;
-              //ignoramos os espacos em branco
-              if(ch != ' '){
-                 //Pegamos a quebra de linha, sendo que esta define o final de uma equacao
-                if(ch == '\n'){
-                  linha++;
-                  //Volta ini para 1 pois e uma nova linha
-                  ini=1;
-                  //o incremento ocorre no final do laco, entao setamos para 0 para ele ser incrementado para 1
-                  i=0;
-                }
-                else{
-                    //Adicionamos o simbolo, problema é que é necessário converter char para string porque o java nao faz automaticamente
-                    tokens.add(new Token(String.valueOf(ch),linha,ini,i));
-                    //Ini recebe i+1 pois estamos preparando o ini do proximo numero
-                    ini = i+1;
-                }
-              }
-          }
-          i++;
-        }
-        //Repetimos esse trecho pois o ultimo valor pode ser um numero nao inserido na tabela
-        if(tmp != "")
-            tokens.add(new Token(tmp,linha,ini,i-1));
+        //Recebe uma lista de tokens 
+        List<Token> tokens = al.analisa(str);
         
         //Construimos novamente a tabela de simbolos, para poder preenche-la no for abaixo
         DefaultTableModel modelo = new javax.swing.table.DefaultTableModel();
